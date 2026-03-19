533a54c04af93cb4ee57c34012268fc65ee50da3
emanmz <emanzaheer@utexas.edu>
2026-03-19 15:17:14 -0500

    fixed j body and j space

--- Functions/J_body.m
+++ Functions/J_body.m
@@ -1,26 +1,25 @@
-
+% EE to base
 function Jb = J_body(Bn, theta) % W7-L1-SL6
 
 % n = number of joints
 % Bn = 6xn matrix of screw axis in body frame
 % theta = 1xn matrix of joint angles
 
-Adj = eye(6);
 n = length(theta);
 Jb = zeros(6,n);
+
+% last is last screw
 Jb(:,n) = Bn(:,n);
 
 for i = n-1:-1:1
 
     % calculate transformation for ith joint
-    Ti_prev = screw_to_exp(-Bn(:,i+1),theta(i+1));
-    
-    % change to adjoint
-    Adi_prev = Adjoint(Ti_prev);
+    Ti_next = screw_to_exp(-Bn(:, i+1), theta(i+1));
 
-    % multiply it to previous adjoints
-    Adj = Adj*Adi_prev; 
+    % cumulative transformation
+    T = T * Ti_next;
 
-    % Jacobian column for i+1 column 
-     Jb(:,i) = Adj*Bn(:,i);
-end 
+    % trans the current body screw axis
+    Jb(:, i) = Adjoint(T) * Bn(:, i);
+end
+end
--- Functions/J_space.m
+++ Functions/J_space.m
@@ -1,51 +1,49 @@
-
+% base 2 joint
 function Js = J_space(Sn, theta) % W7-L1-SL6
 
 % n = number of joints
 % Sn = 6xn matrix of screw axis in space frame
 % theta = 1xn matrix of joint angles
 
-Adj = eye(6);
 n = length(theta);
 Js = zeros(6,n);
+% first column is screw
 Js(:,1) = Sn(:,1);
 
 for i = 2:n
 
     % calculate transformation for ith joint
     Ti_prev = screw_to_exp(Sn(:,i-1),theta(i-1));
-    
-    % change to adjoint
-    Adi_prev = Adjoint(Ti_prev);
 
-    % multiply it to previous adjoints
-    Adj = Adj*Adi_prev; 
+    T = T * Ti_prev;
 
-    % Jacobian column for i+1 column 
-     Js(:, i) = Adj*Sn(:,i);
-end 
+    % Column i is the screw axis i transformed by the Adjoint of T
 
-function Js = J_space(Sn, theta)
+    Js(:, i) = Adjoint(T) * Sn(:, i);
+end
+end
 
-% Sn = matrix of screw axis in home position
-% theta = matrix of joint angles
-
-Adj = eye(6);
-n = length(theta);
-Js = zeros(6,n);
-Js(:,1) = Sn(:,1);
-
-for i = 2:n
-
-    % calculate transformation for ith joint
-    Ti_prev = screw_to_exp(Sn(:,i-1),theta(i-1));
-    
-    % change to adjoint
-    Adi_prev = Adjoint(Ti_prev);
-
-    % multiply it to previous adjoints
-    Adj = Adj*Adi_prev; 
-
-    % Jacobian column for i+1 column 
-     Js(:, i) = Adj*Sn(:,i);
-end 
+% function Js = J_space(Sn, theta)
+% 
+% % Sn = matrix of screw axis in home position
+% % theta = matrix of joint angles
+% 
+% Adj = eye(6);
+% n = length(theta);
+% Js = zeros(6,n);
+% Js(:,1) = Sn(:,1);
+% 
+% for i = 2:n
+% 
+%     % calculate transformation for ith joint
+%     Ti_prev = screw_to_exp(Sn(:,i-1),theta(i-1));
+% 
+%     % change to adjoint
+%     Adi_prev = Adjoint(Ti_prev);
+% 
+%     % multiply it to previous adjoints
+%     Adj = Adj*Adi_prev; 
+% 
+%     % Jacobian column for i+1 column 
+%      Js(:, i) = Adj*Sn(:,i);
+% end 
