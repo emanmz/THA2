4d118f2b477749c670002b43a26e61a93205fedf
sjcheatham <sjcheatham93@gmail.com>
2026-03-21 18:55:57 -0500

    cleaned up a bit

--- FK_test.m
+++ FK_test.m
@@ -20,13 +20,15 @@ M_wam = [1 0 0 0;
 Blist_wam = zeros(6, 7);
 for i = 1:7
     wi = wb{i};
-    vi = cross(wi, -qb{i}); % Body screw v calculation
+    vi = cross(-wi, qb{i}); % Body screw v calculation
     Blist_wam(:, i) = [wi; vi];
 end
 
 
 % Forward Kinematics (Body)
 Tb_WAM = FK_body(M_wam, Blist_wam, theta_wam);
+disp('Tb_WAM')
+disp(Tb_WAM)
 
 %  derive Slist from Blist using the Adjoint of M
 Slist_wam = zeros(6, 7);
@@ -37,11 +39,15 @@ end
 % done! 
 
 % Forward Kinematics (Space)
+
 Ts_WAM = FK_space(M_wam, Slist_wam, theta_wam);
+disp('Ts_WAM')
+disp(Ts_WAM)
 
 % Check equivalence
 error_wam = norm(Ts_WAM - Tb_WAM);
 fprintf('WAM Space/Body Equivalence Error: %e\n', error_wam);
+
 %% FK Test Cases
 
 % W6-L1-SL11 3R Example
@@ -50,8 +56,8 @@ L1 = 2;
 L2 = 3;
 M_3r = [0 0 1 L1; 0 1 0 0; -1 0 0 -L2; 0 0 0 1];
 w_3r = {[0;0;1], [0;-1;0], [1;0;0]};
-q_3r = {[0;0;0], [0;L1;0], [0;0;-L2]};
-theta_3r = [pi/2, pi, 3*pi/2];
+q_3r = {[0;0;0], [L1;0;0], [0;0;-L2]};
+theta_3r = [0, pi/2, 0 ];
 
 % Convert to Slist
 Slist_3r = zeros(6, 3);
@@ -62,13 +68,18 @@ for i = 1:3
 end
 
 Ts_3r = FK_space(M_3r, Slist_3r, theta_3r);
+disp('Ts_3r')
+disp(Ts_3r)
 
 % Derive Blist for 3R to check the other way
 Blist_3r = zeros(6, 3);
 for i = 1:3
     Blist_3r(:, i) = Adjoint(inv(M_3r)) * Slist_3r(:, i);
 end
+
 Tb_3r = FK_body(M_3r, Blist_3r, theta_3r);
+disp('Tb_3r')
+disp(Tb_3r)
 
 fprintf('3R Space/Body Equivalence Error: %e\n', norm(Ts_3r - Tb_3r));
 
