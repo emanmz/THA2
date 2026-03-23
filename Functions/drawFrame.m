ebc4117ef3bb4b95b557c54b0e41f1159e04f884
emanmz <emanzaheer@utexas.edu>
2026-03-23 18:25:11 -0500

    Update drawFrame.m
    scaling thing

--- Functions/drawFrame.m
+++ Functions/drawFrame.m
@@ -5,7 +5,7 @@ function drawFrame(T, scale, label)
     R = T(1:3, 1:3);
     colors = ['r', 'g', 'b']; % X=Red, Y=Green, Z=Blue
     for i = 1:3
-        quiver3(pos(1), pos(2), pos(3), R(1,i), R(2,i), R(3,i), scale, ...
+        quiver3(pos(1), pos(2), pos(3), R(1,i)*scale, R(2,i)*scale, R(3,i)*scale, ...
                 'Color', colors(i), 'LineWidth', 2);
     end
     text(pos(1), pos(2), pos(3), label, 'FontSize', 10, 'FontWeight', 'bold');
